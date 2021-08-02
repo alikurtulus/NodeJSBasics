c4f9e144313a0cab42951c62f1018ea36ad2309e
alikurtulush92 <kopanarya92@gmail.com>
2021-08-02 21:08:06 +0100

    added node js app

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const http = require('http');
-const {handler} = require('./routes/routes')
+// const http = require('http');
+// const {handler} = require('./routes/routes')
+const bodyParser = require('body-parser')
+const express = require('express')
+const path = require('path')
+const app = express()
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-const server = http.createServer(handler)
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin',adminRoutes)
+app.use(shopRoutes)
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname,'./','views','404.html'));
+})
+
+// const server = http.createServer(app)
+// server.listen(3000);
+app.listen(3000)
