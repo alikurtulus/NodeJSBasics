ba868da2884506bc3168086fb77074360c3d63cb
alikurtulush92 <kopanarya92@gmail.com>
2021-08-08 00:21:20 +0100

    added controllers product model and helpers

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product',{pageTitle:'Add Product', path:'/admin/add-product'} )
+    res.render('admin/add-product',{pageTitle:'Add Product', path:'/admin/add-product'} )
 }
 
 exports.postAddProduct = (req, res, next) => {
@@ -12,7 +12,7 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll( products => {
-        res.render('shop', {prods: products, docTitle:'Shop', path:'/', pageTitle:'Shop'})
+        res.render('shop/product-list', {prods: products, docTitle:'Shop', path:'/', pageTitle:'Shop'})
     });
    
 }
